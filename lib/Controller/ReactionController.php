38af2df3fe976000e5b11231a793b8776cd9ff7c
Vitor Mattos <vitor@php.rio>
2021-12-24 11:24:02 -0300

    Api changes to react to a message: reactions retrieve
    Signed-off-by: Vitor Mattos <vitor@php.rio>

--- lib/Controller/ReactionController.php
+++ lib/Controller/ReactionController.php
@@ -26,11 +26,13 @@ declare(strict_types=1);
 namespace OCA\Talk\Controller;
 
 use OCA\Talk\Chat\CommentsManager;
+use OCA\Talk\Chat\MessageParser;
 use OCA\Talk\Chat\ReactionManager;
 use OCP\AppFramework\Http;
 use OCP\AppFramework\Http\DataResponse;
 use OCP\AppFramework\Utility\ITimeFactory;
 use OCP\Comments\NotFoundException;
+use OCP\IL10N;
 use OCP\IRequest;
 
 class ReactionController extends AEnvironmentAwareController {
@@ -40,17 +42,25 @@ class ReactionController extends AEnvironmentAwareController {
 	private $commentsManager;
 	/** @var ReactionManager */
 	private $reactionManager;
+	/** @var IL10N */
+	private $l;
+	/** @var MessageParser */
+	private $messageParser;
 
 	public function __construct(string $appName,
 								IRequest $request,
 								ITimeFactory $timeFactory,
 								CommentsManager $commentsManager,
-								ReactionManager $reactionManager) {
+								ReactionManager $reactionManager,
+								IL10N $l,
+								MessageParser $messageParser) {
 		parent::__construct($appName, $request);
 
 		$this->timeFactory = $timeFactory;
 		$this->commentsManager = $commentsManager;
 		$this->reactionManager = $reactionManager;
+		$this->l = $l;
+		$this->messageParser = $messageParser;
 	}
 
 	/**
@@ -120,5 +130,41 @@ class ReactionController extends AEnvironmentAwareController {
 		}
 
 		return new DataResponse([], Http::STATUS_CREATED);
+	}
+
+	/**
+	 * @NoAdminRequired
+	 * @RequireParticipant
+	 * @RequireReadWriteConversation
+	 * @RequireModeratorOrNoLobby
+	 *
+	 * @param int $messageId for reaction
+	 * @param string $emoji the reaction emoji
+	 * @return DataResponse
+	 */
+	public function getReactions(int $messageId, string $emoji): DataResponse {
+		try {
+			// Verify if messageId is of room
+			$this->commentsManager->getComment($this->getRoom(), (string) $messageId);
+		} catch (NotFoundException $e) {
+			return new DataResponse([], Http::STATUS_NOT_FOUND);
+		}
+
+		$comments = $this->commentsManager->retrieveCommentsOfReaction($messageId, $emoji);
+
+		$reactions = [];
+		foreach ($comments as $comment) {
+			$message = $this->messageParser->createMessage($this->getRoom(), $this->getParticipant(), $comment, $this->l);
+			$this->messageParser->parseMessage($message);
+
+			$reactions[] = [
+				'actorType' => $comment->getActorType(),
+				'actorId' => $comment->getActorId(),
+				'actorDisplayName' => $message->getActorDisplayName(),
+				'timestamp' => $comment->getCreationDateTime()->getTimestamp(),
+			];
+		}
+
+		return new DataResponse($reactions, Http::STATUS_OK);
 	}
 }
